f8e59396250df12b8ce7042b18a1eb05cb4fcfda
youshengchang <changyousheng@yahoo.com>
2014-08-31 16:07:11 -0700

    Initial Commit

---
+++ MatchedUp/ITAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ITAppDelegate.h
+//  MatchedUp
+//
+//  Created by yousheng chang on 8/31/14.
+//  Copyright (c) 2014 InfoTech Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ITAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MatchedUp/ITSecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  ITSecondViewController.m
+//  MatchedUp
+//
+//  Created by yousheng chang on 8/31/14.
+//  Copyright (c) 2014 InfoTech Inc. All rights reserved.
+//
+
+#import "ITSecondViewController.h"
+
+@interface ITSecondViewController ()
+
+@end
+
+@implementation ITSecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
